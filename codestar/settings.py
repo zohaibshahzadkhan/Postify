e2df6e2ac89d75d0521d901fadfb89834fa34be5
Zohaib Shahzad <zohaibshahzad16@gmail.com>
2024-04-26 17:29:33 +0200

    Read secret from local env

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lzb$%f+0h7$kd-g8a8z@vh9mujy!9%uw=c!r8!g-0q2km_5+ms'
+# SECRET_KEY = 'django-insecure-lzb$%f+0h7$kd-g8a8z@vh9mujy!9%uw=c!r8!g-0q2km_5+ms'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
